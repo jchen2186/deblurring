aacad8173ee35a7ad1b6b45f02c9f14aed928044
timashana <atimash000@citymail.cuny.edu>
2018-05-17 14:58:35 -0400

    tried for bigger picture

--- deblurring.py
+++ deblurring.py
@@ -7,17 +7,17 @@ def deblur(y):
     """
 
     # applying the formula (5) from the paper
-    x = (H + lam*I).T*H*y
+    x = (H + lam*I).I*y
 
     return x
 
 def gaussian_blur(img,sigma):
 	#blur the image using Gaussian Blur; specify a kernel (odd positive) and sigma
-	blurred = cv.GaussianBlur(img, (65,65), sigma)
+	blurred = cv.GaussianBlur(img, (51,51), sigma)
 	return blurred
 
 # we're working with 255 x 255 pixel images
-image_size = 100 * 100
+image_size = 255 * 255
 
 # Identity matrix I
 I = np.identity(image_size)
@@ -41,21 +41,27 @@ identity = np.matrix(np.identity(image_size))
 
 # import image
 img1 = cv.imread('img/flower1.jpg')
-img1_blurred = gaussian_blur(img1,7)
+img1_blurred = gaussian_blur(img1,3)
 cv.imwrite("img/img1_blurred7.jpg", img1_blurred)
 
-
-
 # split image into different color planes
-b, g, r = cv.split(img1)
+b, g, r = cv.split(img1_blurred)
 
 # flatten the image
 b_flat, g_flat, r_flat = b.flatten(), g.flatten(), r.flatten()
 
 # type cast the lists so they become matrices
-b_flat = np.matrix(b_flat)
-g_flat = np.matrix(g_flat)
-r_flat = np.matrix(r_flat)
+b_flat = np.matrix(b_flat).T
+g_flat = np.matrix(g_flat).T
+r_flat = np.matrix(r_flat).T
+
+r_deblur= deblur(r_flat)
+g_deblur = deblur(g_flat)
+b_deblur = deblur(b_flat)
+
+r_deblur=np.reshape(r_deblur, (255, 255))
+g_deblur=np.reshape(g_deblur, (255, 255))
+b_deblur=np.reshape(b_deblur, (255, 255))
 
 # print(type(b_flat))
 
@@ -66,4 +72,5 @@ r_flat = np.matrix(r_flat)
 # b_deblur = b_flat
 
 # join the color planes to reform the image
-# img = cv.merge((b_deblur, g_deblur, r_deblur))
+img = cv.merge((b_deblur, g_deblur, r_deblur))
+cv.imwrite("img/img1_deblurred3.jpg", img)
